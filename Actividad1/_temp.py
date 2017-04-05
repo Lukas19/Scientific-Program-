6fc6197565e07aca725d0bbe1cafdc89fe6421a0
lukas19 <lzamora@alumnos.inf.utfsm.cl>
2017-04-04 23:14:48 -0300

    Actividad Clase 1

---
+++ Actividad1/_temp.py
@@ -0,0 +1,12 @@
+a = float(raw_input("Ingrese a: "))
+b = float(raw_input("Ingrese b: "))
+c = float(raw_input("Ingrese c: "))
+
+if a>(b+c) or b>(a+c) or c>(a+b):
+    print(a = float(raw_input("Ingrese a: ")))
+elif a==b and b==c:
+    print(a = float(raw_input("Ingrese a: ")))
+elif a==b or b==c or a==c:
+    print(a = float(raw_input("Ingrese a: ")))
+else:
+    print(a = float(raw_input("Ingrese a: ")))
